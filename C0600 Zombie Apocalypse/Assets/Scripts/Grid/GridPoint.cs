0add6e2047de3013d49b2a789992ca2861ff2445
kasim-hussain <mkasim.hussain@outlook.com>
2019-12-09 13:46:09 +0000

    Grid Update
    Added Point and Wall highlighting, along with placing Turrets.

---
+++ C0600 Zombie Apocalypse/Assets/Scripts/Grid/GridPoint.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPoint : MonoBehaviour
+{
+    SpriteRenderer pointSprite;
+    Color defaultColor;
+
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public void SetPoint(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    void Start()
+    {
+        pointSprite = GetComponent<SpriteRenderer>();
+        defaultColor = pointSprite.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        pointSprite.color = Color.blue;
+    }
+
+    private void OnMouseExit()
+    {
+        pointSprite.color = defaultColor;
+    }
+
+    public void MakeTurret()
+    {
+    }
+}
